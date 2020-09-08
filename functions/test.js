dc7d510776b7470434838a7ef1bc3bd44f8dfa8c
Sami Koskivaara <sami.koskivaara@gmail.com>
2020-09-08 13:13:44 +0300

    first commit

--- functions/test.js
+++ functions/test.js
@@ -1,12 +1,18 @@
 exports.handler = function(event, context, callback) {
-  const { name } = JSON.parse(event.body)
-
-  if (name === 'james') {
-    callback(null, {
-      statusCode: 200,
-      body: JSON.stringify({ msg: `Thanks for visiting ${name}` }),
-    })
-  } else {
-    callback(new Error("You're not James!"))
+  let statusCode
+  let body
+  try {
+    const parsedBody = JSON.parse(event.body)
+    const name = parsedBody.name || 'Unknown'
+    statusCode = 200
+    body = JSON.stringify({ msg: `Thanks for visiting ${name}` })
+  } catch (err) {
+    statusCode = 400
+    body = JSON.stringify({ msg: 'Error, parsing post data' })
   }
+
+  callback(null, {
+    statusCode,
+    body,
+  })
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,9 +5,8 @@ import Layout from '../components/layout'
 
 const IndexPage = () => (
   <Layout>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <h1>Gatsby Header</h1>
+    <p>This is a Gatsby default site + functions.</p>
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
